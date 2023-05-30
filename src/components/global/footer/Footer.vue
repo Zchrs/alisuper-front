18bab172ce3ff18051edf79bcd952838584625da
Zchack <gustavo.a.c.m@gmail.com>
2023-05-30 17:41:19 -0500

    cardsss

--- src/components/global/footer/Footer.vue
+++ src/components/global/footer/Footer.vue
@@ -4,34 +4,42 @@
       <a class="footer-up__a" href="#">Volver arriba</a>
     </div>
     <div class="footer-middle">
+      <div class="footer-middle__contain">
+        <div class="footer-middle__links">
+          <h2>Alisuper</h2>
+          <a href="">Trabaja con nosotros</a>
+          <a href="">Sobre nosotros</a>
+          <a href="">Blog</a>
+          <a href="">Tecnología</a>
+        </div>
+      
       <div class="footer-middle__links">
-        <h2>Alisuper</h2>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
+        <h2>Gana con Alisuper</h2>
+        <a href="">Proveedor Alisuper</a>
+        <a href="">Programa de afiliados</a>
+        <a href="">Vender aplicaciones</a>
+        <a href="">Anuncia con nosotros</a>
+        <a href="">Super deliver</a>
       </div>
       <div class="footer-middle__links">
-        <h2>Gana dinero con Alisuper</h2>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-      </div>
-      <div class="footer-middle__links">
-        <h2>Membresías</h2>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
+        <h2>Productos ágiles</h2>
+        <a href="">Redimir puntos</a>
+        <a href="">Recargar saldo</a>
+        <a href="">Convertidor de divisas (proximamente)</a>
+        <a href="">Comprar ALS (Alisuper Token)</a>
+        <a href="">Comprar con ALS</a>
+        <a href="">Comprar NFT</a>
       </div>
       <div class="footer-middle__links">
         <h2>Centro de ayuda</h2>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
-        <a href="">link footer</a>
+        <a href="">Preguntas frecuentes</a>
+        <a href="">Contacto</a>
+        <a href="">Haznos un (PQRS)</a>
+        <a href="">Políticas de cambio y devoluciones</a>
+        <a href="">Cambios y devoluciones</a>
+        <a href="">Actualizar datos.</a>
       </div>
+    </div>
     </div>
     <div class="footer-down">
       <h1>Footer</h1>
@@ -61,24 +69,35 @@
   }
   &-middle{
     display: grid;
-    place-items: center;
-    grid-template-columns: repeat(4, 1fr);
+    padding: 24px;
     background: #4c4747;
-    height: fit-content;
+    place-items: center;
 
-    @media (max-width: 700px) {
-      grid-template-columns: repeat(2, 1fr);
+    &__contain{
+      display: grid;
+      width: 75%;
+      grid-template-columns: repeat(4, 1fr);
+      height: fit-content;
+      gap: 12px;
+      
+          @media (max-width: 700px) {
+            grid-template-columns: repeat(2, 1fr);
+            width: 100%;
+          }
     }
 
     &__links{
+      
+      width: 100%;
       display: grid;
-      padding: 12px;
+      height: fit-content;
+      
       gap: 10px;
       a{
         color: white;
         text-decoration: none;
         font-size: 16px;
-        font-weight: 300;
+        font-weight: 200;
       }
       h2{
         font-weight: 500;
